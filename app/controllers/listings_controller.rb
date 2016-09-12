6815d8ce85724211511ed85bc9ad13f6cb620407
bogdan <srk.bogdan@gmail.com>
2016-09-12 10:24:17 +0300

    subcategories links

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,3 +1,24 @@
 class ListingsController < ApplicationController
+  def new
+    @listing = Listing.new
+  end
 
-end
+  def create
+    @listing = Listing.new(listing_params)
+
+    unless @listing.save
+      puts '===================='
+      logger.debug @listing.errors.inspect
+    end
+    redirect_to root_path
+  end
+
+    def show
+      @listing = Listing.find(params[:id])
+    end
+
+    private
+    def listing_params
+      params.require(:listing).permit(:title, :description, :city, :state, :zipcode)
+    end
+  end
