5be2ceb38a0ce1720bf50e08be4cd4e608c76472
bogdan <srk.bogdan@gmail.com>
2016-09-14 12:35:29 +0300

    subcategories form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,9 +8,6 @@ class ListingsController < ApplicationController
 
     if @listing.save
       redirect_to root_path
-    else
-      puts '===================='
-      logger.debug @listing.errors.inspect
     end
 
   end
@@ -21,6 +18,6 @@ class ListingsController < ApplicationController
 
     private
     def listing_params
-      params.require(:listing).permit(:title, :description, :city, :state, :zipcode)
+      params.require(:listing).permit(:title, :description, :city, :state, :zipcode, :category_id, :subcategory_id)
     end
   end
--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -4,4 +4,10 @@ class SubcategoriesController < ApplicationController
 
   end
 
+  def find_by_category
+    category = Category.find(params[:category_id])
+    subcategories = category.subcategories.find_all
+    render json: {subcategories: subcategories}
+  end
+
 end
